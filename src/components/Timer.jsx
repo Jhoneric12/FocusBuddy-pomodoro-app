5ffb0b0c3bcae9ef247a13a15290cc7dec2852d7
unknown <jhonerix23@gmail.com>
2024-05-24 22:41:56 +0800

    modified timers

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,28 +3,49 @@ import React, { useState, useEffect } from 'react';
 function Timer() {
 
   let timer;
-  const initialTime = 25 * 60;
-  const [time, setTime] = useState(initialTime);
+  let short;
+  let long;
+  let timerContent;
+  let pomodoroTimer = 25 * 60;
+  let shortBreakTimer = 5 * 60;
+  let longBreakTimer = 10 * 60;
+  const [time, setTime] = useState(pomodoroTimer);
+  const [shortTime, setShortTime] = useState(shortBreakTimer);
+  const [longTime, setLongTime] = useState(longBreakTimer);
   const [isRunning, setIsRunning] = useState(false);
+  const [tab, setTab] = useState(1);
 
-  // When the timer starts
-  const startTimer = () => {
-    setIsRunning(true);
-  };
-
-  const resetTimer = () => {
-    clearInterval(timer);
-    setTime(initialTime);
-    setIsRunning(false);
+  // Conditional Rendering 
+  if (tab === 1) {
+    timerContent = (
+        <h1 className='font-bold text-[5rem] text-font-color'>
+            {Math.floor(time / 60).toString().padStart(2, '0')}:{(time % 60).toString().padStart(2, '0')}
+        </h1>
+    );
   }
 
-  useEffect(() => {
+  else if (tab === 2 ) {
+    timerContent = (
+        <h1 className='font-bold text-[5rem] text-font-color'>
+            {Math.floor(shortTime / 60).toString().padStart(2, '0')}:{(shortTime % 60).toString().padStart(2, '0')}
+        </h1>
+    );
+  }
 
-    if (isRunning) {
-       timer = setInterval(() => {
+  else if (tab === 3 ) {
+    timerContent = (
+        <h1 className='font-bold text-[5rem] text-font-color'>
+            {Math.floor(longTime / 60).toString().padStart(2, '0')}:{(longTime % 60).toString().padStart(2, '0')}
+        </h1>
+    );
+  }
+
+  const pomodoro = () => {
+    timer = setInterval(() => {
         setTime(prevTime => {
           if (prevTime <= 0) {
             clearInterval(timer);
+            setTime(pomodoroTimer);
             setIsRunning(false);
             return 0;
           } 
@@ -33,18 +54,110 @@ function Timer() {
           }
         });
       }, 1000);
+  }
+
+  const shortBreak = () =>  {
+    short = setInterval(() => {
+        setShortTime(prevTime => {
+          if (prevTime <= 0) {
+            clearInterval(short);
+            setShortTime(shortBreakTimer);
+            setIsRunning(false);
+            return 0;
+          } 
+          else {
+            return prevTime - 1;
+          }
+        });
+      }, 1000);
+  }
+
+  const longBreak = () =>  {
+    long = setInterval(() => {
+        setLongTime(prevTime => {
+          if (prevTime <= 0) {
+            clearInterval(long);
+            setLongTime(longBreakTimer);
+            setIsRunning(false);
+            return 0;
+          } 
+          else {
+            return prevTime - 1;
+          }
+        });
+      }, 1000);
+  }
+
+  // When the timer starts
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  // When the timer resets
+  const resetTimer = () => {
+    clearInterval(timer);
+    setTime(pomodoroTimer);
+    setShortTime(shortBreakTimer);
+    setLongTime(longBreakTimer);
+    setIsRunning(false);
+  }
+
+  // Tabs
+  const toggle = (id) => {
+    setIsRunning(false);
+    setTab(id);
+  }
+
+  // Pomodoro Timer
+  useEffect(() => {
+
+    if (isRunning) {
+       if (tab === 1) {
+        pomodoro();
+       }
     }
 
-    return () => clearInterval(timer);
+   return () => clearInterval(timer);
 
   }, [isRunning]);
+
+  // Short Break
+  useEffect(() => {
+
+    if (isRunning) {
+       if (tab === 2) {
+        shortBreak();
+       }
+    }
+
+   return () => clearInterval(short);
+
+  }, [isRunning]);
+
+  // Long Break
+  useEffect(() => {
+
+    if (isRunning) {
+       if (tab === 3) {
+        longBreak();
+       }
+    }
+
+   return () => clearInterval(long);
+
+  }, [isRunning]);
+
+
 
   return (
     <div className='select-none'>
       <div className='flex items-center flex-col'>
-        <h1 className='font-bold text-[5rem] text-font-color'>
-          {Math.floor(time / 60).toString().padStart(2, '0')}:{(time % 60).toString().padStart(2, '0')}
-        </h1>
+        <div className='flex gap-4 text-[0.875rem]'>
+            <button className='text-font-color focus:bg-secondary-color rounded-md p-2' onClick={() => toggle(1)}>Pomodoro</button>
+            <button className='text-font-color focus:bg-secondary-color rounded-md p-2' onClick={() => toggle(2)}>Short Break</button>
+            <button className='text-font-color focus:bg-secondary-color rounded-md p-2' onClick={() => toggle(3)}>Long Break</button>
+        </div>
+        {timerContent}
         {
             isRunning
             ? 
